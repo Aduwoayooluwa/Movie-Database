28ebd37b523ce649a9c734724366e724da987bbd
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-03-20 10:41:04 +0100

    dynamic routing

--- src/components/GetData.tsx
+++ src/components/GetData.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 
+
 type Props = {
     data: any
-    pageNumber: number
+    category: string
 }
 
-const GetData = ({ data, pageNumber }: Props) => {
+const GetData = ({ data, category }: Props) => {
+
+
     return (
         <div className="">
             <Swiper
@@ -18,12 +21,15 @@ const GetData = ({ data, pageNumber }: Props) => {
             data?.results.map((item, index) => {
                 return (
                     <SwiperSlide key={index}>
-                        <div  className='my-4 p-2'>
-                            <img className="rounded" src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path || item.profile_path || item.postal_path}`} alt="images" />
-                            <p className="border mt-2 border-slate-50 rounded-md hover:bg-slate-500 w-fit p-1 text-xs">Date: {item.release_date}</p>
-                            <p className='font-semibold text-lg my-2'>{item.title || item.original_name || item.name}</p>
-                            {/* <p className='text-sm mb-2'>{item.overview}</p>                         */}
-                        </div>
+                        <a href={`${category}/${item.id}`}>
+                            <div  className='my-4 p-2'>
+                                <img className="rounded" src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path || item.profile_path || item.postal_path}`} alt="images" />
+                                <p className="border mt-2 border-slate-50 rounded-md hover:bg-slate-500 w-fit p-1 text-xs">Date: {item.release_date}</p>
+                                <p className='font-semibold text-lg my-2'>{item.title || item.original_name || item.name}</p>
+                                {/* <p className='text-sm mb-2'>{item.overview}</p>                         */}
+                            </div>
+                        </a>
+                        
                     </SwiperSlide>
                 )
             })
---
+++ src/components/GetDetails.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+
+
+type Props = {
+    data: any
+}
+
+const GetDetails = ({ data }: Props) => {
+
+
+    return (
+        <div className="">
+            {/* { JSON.stringify(data) } */}
+            <section>
+            <img className="rounded mt-3" src={`https://image.tmdb.org/t/p/w500/${data.backdrop_path || data.profile_path || data.postal_path}`} alt="images" />
+            <p className="border mt-2 border-slate-50 rounded-md hover:bg-slate-500 w-fit p-1 text-xs">Released on: {data.release_date}</p>
+            <p className='my-2 font-semibold text-xl'>{data.original_title}</p>
+
+            <summary className='my-3'>{data.overview}</summary>
+            <div className='flex items-center'>
+                {
+                data.genres.map((item, index: number) => {
+                    return (
+                        <div key={index} className='border p-2 hover:bg-slate-600 rounded w-fit'>
+                            <p className='text-xs'>{item.name}</p>
+                        </div>
+                    )
+                })
+            }
+            </div>
+
+            <div className='w-full my-4  md:w-1/3'>
+                <p className='font-semibold mb-2'>Production Companies: </p>
+                <div className='grid w-1/2 gap-2 grid-cols-3 place-items-center'>
+                    {
+                data.production_companies.map((item, index: number) => {
+                    return (
+                        <div key={index} className='border p-2 hover:bg-slate-600 rounded w-fit'>
+                            <p className='text-xs'>{item.name}</p>
+                        </div>
+                    )
+                })
+            }
+                </div>
+                
+            </div>
+
+            <div className='w-full md:w-1/3'>
+                <p className='font-semibold mb-2'>Production Countries: </p>
+                <div className='grid w-1/2 gap-2 grid-cols-3 place-items-center'>
+                    {
+                data.production_countries.map((item, index: number) => {
+                    return (
+                        <div key={index} className='border p-2 hover:bg-slate-600 rounded w-fit'>
+                            <p className='text-xs'>{item.name}</p>
+                        </div>
+                    )
+                })
+            }
+                </div>
+                
+            </div>
+            
+            </section>
+        
+            </div>
+    )
+}
+
+export default GetDetails
